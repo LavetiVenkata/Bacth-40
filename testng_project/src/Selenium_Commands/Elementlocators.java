5c1dccb1ad7e7883c05283dbce80753dec699920
LavetiVenkata <dsatyaveni@gmail.com>
2019-05-13 07:47:40 +0530

    local commit

--- testng_project/src/Selenium_Commands/Elementlocators.java
+++ testng_project/src/Selenium_Commands/Elementlocators.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Elementlocators {
 
-	WebDriver driver;
+	public static WebDriver driver;
 
 	public void invokeBrowser(String url)
 	{
